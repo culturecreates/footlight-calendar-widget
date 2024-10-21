d0af2aaebb5aae377c06af1f8c51d534eb9dd961
syam babu <syam@dexlock.com>
2024-10-21 15:15:06 +0530

    fix: removed deletion of previous versions during major release. reomved some global css. changed .js and .css file name

--- config-overrides.js
+++ config-overrides.js
@@ -3,13 +3,13 @@ const { version } = require('./package.json');
 const [major] = version.split('.');
 
 module.exports = function override(config, env) {
-  config.output.filename = `static/js/widget-${major}.js`;
+  config.output.filename = `static/js/widget.js`;
 
   const miniCssExtractPlugin = config.plugins.find(
     (plugin) => plugin.constructor.name === 'MiniCssExtractPlugin',
   );
   if (miniCssExtractPlugin) {
-    miniCssExtractPlugin.options.filename = `static/css/widget-${major}.css`;
+    miniCssExtractPlugin.options.filename = `static/css/widget.css`;
   }
 
   return config;
