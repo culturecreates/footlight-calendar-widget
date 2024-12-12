e4a33ef38509173a14951e745f512b87859b5a2a
syam babu <syam@dexlock.com>
2024-12-12 15:58:08 +0530

    Disabled "See all" button in footer for External redirection type widgets

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,12 +3,13 @@ import { useTranslation } from 'react-i18next';
 import WidgetContext from '../../context/WidgetContext';
 import './footer.css';
 import { redirectionHandler } from '../../utils/redirectionHandler';
+import { redirectionModes } from '../../constants/generalConstants';
 
 const Footer = () => {
   const { t } = useTranslation();
   const { widgetProps, totalCount } = useContext(WidgetContext);
 
-  const { calendar, calendarName, locale } = widgetProps;
+  const { calendar, calendarName, locale, redirectionMode } = widgetProps;
   let redirectionUrl = `${process.env.REACT_APP_API_URL}calendars/${calendar}/events/redirect?locale=${locale}`;
 
   const submitHandler = (event) => {
@@ -18,9 +19,11 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <div className="button-container" onClick={submitHandler}>
-        <button>{totalCount > 0 ? t('footer.text') : t('footer.noItems')}</button>
-      </div>
+      {redirectionMode == redirectionModes.EXTERNAL && (
+        <div className="button-container" onClick={submitHandler}>
+          <button>{totalCount > 0 ? t('footer.text') : t('footer.noItems')}</button>
+        </div>
+      )}
       <div className="provided-by-container">
         <span className="text">{t('footer.providedBy')}</span>
         <span className="calendar-name">{calendarName}</span>
