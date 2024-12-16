e2684c2945bd42b0e05afbf86405837238aa2f80
syam babu <syam@dexlock.com>
2024-12-16 18:05:50 +0530

    read-only modal added - added a read-only modal for displaying event details

--- src/utils/dynamicStylePropertyInjector.js
+++ src/utils/dynamicStylePropertyInjector.js
@@ -1,12 +1,6 @@
-export const dynamicCssColorInjector = (palette) => {
-  const dynamicColorVariableNames = {
-    50: '--dynamic-color-50',
-    100: '--dynamic-color-100',
-    500: '--dynamic-color-500',
-    600: '--dynamic-color-600',
-    700: '--dynamic-color-700',
-  };
+import { dynamicColorVariableNames } from '../constants/generalConstants';
 
+export const dynamicCssColorInjector = (palette) => {
   Object.keys(dynamicColorVariableNames).forEach((key) => {
     document.documentElement.style.setProperty(dynamicColorVariableNames[key], palette[key]);
   });
