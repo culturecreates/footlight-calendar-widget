7d877baea04f505457d43a760cba5f1e87652e33
Abhishek P Anil <abhishek@culturecreates.com>
2025-02-03 23:21:06 +0530

    feat: added max and min width to card and icon for location

--- src/components/card/EventCard/EventCard.jsx
+++ src/components/card/EventCard/EventCard.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { Box, Image, Heading, Stack, Text, useBreakpointValue, Flex } from '@chakra-ui/react';
 import EventTypeBadge from '../../badge/EventTypeBadge/EventTypeBadge';
 import DateBadge from '../../badge/DateBadge/DateBadge';
+import StageIcon from '../../../assets/LocationPin.svg';
 
 const EventCard = ({ event }) => {
   const { image, eventName, stageName, eventType = [], startDate, altText } = event;
 
-  const cardWidth = useBreakpointValue({ base: '100%', md: '48%', lg: '30%' });
+  const cardWidth = useBreakpointValue({ base: '100%', sm: '48%', md: '32%', lg: '30%' });
   const imageHeight = useBreakpointValue({ base: '200px', sm: '250px', md: '300px' });
 
   return (
     <Box
-      maxW={cardWidth}
-      width="100%" // Ensure it stretches full width within the cardWidth range
-      maxWidth="370px" // Set maximum width to 370px
-      borderWidth="1px"
+      flex="1 1 auto"
+      width={cardWidth}
+      maxWidth="370px"
+      minWidth="250px"
       borderRadius="10px"
       overflow="hidden"
       boxShadow="sm"
@@ -23,7 +24,7 @@ const EventCard = ({ event }) => {
     >
       <Image src={image} alt={altText} objectFit="cover" width="100%" height={imageHeight} />
 
-      <Box p={4} bg="#F6F6F6">
+      <Box p={4} bg="var(--event-card-bg)">
         <Stack spacing={3}>
           <Heading as="h3" size="md" noOfLines={2} isTruncated>
             {eventName}
@@ -31,17 +32,19 @@ const EventCard = ({ event }) => {
 
           <Flex justify="flex-start" align="center" wrap="nowrap" gap={2}>
             <DateBadge startDate={startDate} />
-
-            <Text
-              fontSize="13px"
-              fontWeight="500"
-              color="#555555"
-              noOfLines={1}
-              isTruncated
-              ml="2px"
-            >
-              {stageName}
-            </Text>
+            <Flex align="center" justify={'center'} ml="2px" gap={1}>
+              <Image src={StageIcon} alt="Stage Icon" mr={1} />
+              <Text
+                fontSize="13px"
+                fontWeight="500"
+                color="#555555"
+                noOfLines={1}
+                isTruncated
+                ml="2px"
+              >
+                {stageName}
+              </Text>
+            </Flex>
           </Flex>
 
           <Flex wrap="wrap" spacing={3} justify="flex-start" gap={2}>
