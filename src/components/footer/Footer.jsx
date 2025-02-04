1d779ffb92b9664d0e551475f2b5153bdc013327
syam babu <syam@dexlock.com>
2025-02-04 13:56:49 +0530

    new footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,33 +1,17 @@
+/* eslint-disable no-unused-vars */
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import WidgetContext from '../../context/WidgetContext';
 import './footer.css';
-import { getRedirectionUrl, redirectionHandler } from '../../utils/redirectionHandler';
-import { redirectionModes, urlTypes } from '../../constants/generalConstants';
 
 const Footer = () => {
   const { t } = useTranslation();
-  const { widgetProps, totalCount } = useContext(WidgetContext);
-
-  const { calendar, calendarName, locale, redirectionMode } = widgetProps;
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    redirectionHandler({
-      url: getRedirectionUrl({ id: null, type: urlTypes.SEARCH_EVENTS, locale, calendar }),
-    });
-  };
+  const { widgetProps } = useContext(WidgetContext);
 
   return (
     <footer className="footer">
-      {redirectionMode == redirectionModes.EXTERNAL && (
-        <div className="button-container" onClick={submitHandler}>
-          <button>{totalCount > 0 ? t('footer.text') : t('footer.noItems')}</button>
-        </div>
-      )}
       <div className="provided-by-container">
         <span className="text">{t('footer.providedBy')}</span>
-        <span className="calendar-name">{calendarName}</span>
         <div className="calendar-logo">
           <img src={widgetProps?.calendarLogo} />
         </div>
