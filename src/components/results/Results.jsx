811179ea34f4dceb7cc19c776e835ec78a9f8aab
syam babu <syam@dexlock.com>
2025-02-04 20:48:17 +0530

    added load more button

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -1,25 +1,39 @@
 import React, { useContext } from 'react';
+import { Button, Icon, VStack } from '@chakra-ui/react';
 import WidgetContext from '../../context/WidgetContext';
-import EventCard from '../card/EventCard/EventCard'; // Import the new EventCard
-import './results.css';
+import EventCard from '../card/EventCard/EventCard';
+import { useTranslation } from 'react-i18next';
+import { ReactComponent as ArrowDownIcon } from '../../assets/ArrowDown.svg';
 
 const Results = () => {
-  const { data } = useContext(WidgetContext);
+  const { data, lastPageFlag, fetchMoreData } = useContext(WidgetContext);
+  const { t } = useTranslation();
 
   return (
-    <ul className="card-container">
-      {data?.length > 0 &&
-        data.map((item, index) => (
-          <EventCard
-            key={index}
-            image={item?.image}
-            eventName={item?.title}
-            stageName={item?.place}
-            eventType={item?.eventTypes}
-            startDate={item?.startDate}
-          />
-        ))}
-    </ul>
+    <VStack spacing={4} overflowY="scroll" height="100%" align="center">
+      {data?.map((item, index) => (
+        <EventCard
+          key={index}
+          image={item?.image}
+          eventName={item?.title}
+          stageName={item?.place}
+          eventType={item?.eventTypes}
+          startDate={item?.startDate}
+        />
+      ))}
+      {lastPageFlag && (
+        <Button
+          variant="link"
+          onClick={fetchMoreData}
+          mt={4}
+          color="var(--strong-grey)"
+          _hover={{ textDecoration: 'underline' }}
+        >
+          {t('loadMore')}
+          <Icon as={ArrowDownIcon} ml={2} />
+        </Button>
+      )}
+    </VStack>
   );
 };
 
