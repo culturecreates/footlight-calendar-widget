7cc648da99e66802fc83c12bd284576cb9c1d100
syam babu <syam@dexlock.com>
2025-02-05 18:30:57 +0530

    added new noresult component

--- src/components/noResult/NoResult.jsx
+++ src/components/noResult/NoResult.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Box, Text, Icon } from '@chakra-ui/react';
 import { ReactComponent as Avatar } from '../../assets/avatar.svg';
-import './noResult.css';
 
 const NoResult = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="no-content">
-      <Avatar />
-      <span>{t('noResults')}</span>
-    </div>
+    <Box
+      width="100%"
+      height="100%"
+      minHeight="96px"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      marginTop="100px"
+    >
+      <Icon as={Avatar} boxSize="40px" />
+      <Text
+        fontSize="var(--secondary-font-weight)"
+        fontWeight="400"
+        lineHeight="16px"
+        textAlign="center"
+        color="var(--primary-grey)"
+      >
+        {t('noResults')}
+      </Text>
+    </Box>
   );
 };
 
