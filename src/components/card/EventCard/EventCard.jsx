fe6ef5eb7c85a3e605c948611fef4959c5503fd8
Abhishek P Anil <abhishek@culturecreates.com>
2025-02-14 13:14:34 +0530

    fix: fixed icon not rendered and added clasnname for event card

--- src/components/card/EventCard/EventCard.jsx
+++ src/components/card/EventCard/EventCard.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import EventTypeBadge from '../../badge/EventTypeBadge/EventTypeBadge';
 import DateBadge from '../../badge/DateBadge/DateBadge';
-import StageIcon from '../../../assets/locationPin.svg';
+import { ReactComponent as StageIcon } from '../../../assets/locationPin.svg';
 import { dateRangeFormatter } from '../../../utils/dateRangeFormatter';
 import './eventCard.css';
+import { Icon } from '@chakra-ui/react';
 
 const EventCard = React.memo(
   ({ image, eventName, stageName, eventType = [], startDate, altText }) => {
-    const cardStyle = {
-      flex: '1 1 auto',
-      maxWidth: '371px',
-      minWidth: '246px',
-      borderRadius: '10px',
-      overflow: 'hidden',
-    };
-
     return (
-      <div className="event-card" style={cardStyle}>
+      <div className="event-card">
         <img src={image} alt={altText} style={{ width: '100%', display: 'block' }} />
 
         <div style={{ padding: '16px', backgroundColor: 'var(--bg-grey)' }}>
@@ -52,7 +45,7 @@ const EventCard = React.memo(
                   marginLeft: '2px',
                 }}
               >
-                <img src={StageIcon} alt="Stage Icon" style={{ width: '14px', height: '14px' }} />
+                <Icon as={StageIcon} alt="Stage icon" style={{ width: '14px', height: '14px' }} />
                 <span
                   style={{
                     fontSize: '13px',
