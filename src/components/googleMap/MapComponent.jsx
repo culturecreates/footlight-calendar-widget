ec0dc7ca4bbfab069cd4138fda249b0346f38cba
Abhishek P Anil <abhishek@culturecreates.com>
2025-02-18 11:31:26 +0530

    feat: updated map component for static map support

--- src/components/googleMap/MapComponent.jsx
+++ src/components/googleMap/MapComponent.jsx
@@ -1,58 +1,23 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { APIProvider, Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
-import { convertDMSStringToDecimal } from '../../utils/convertDMSStringToDecimal';
+import React, { useCallback } from 'react';
 
-const MapComponent = ({ latitude, longitude }) => {
-  const defaultLatLng = { lat: 54.35291352856401, lng: -110.072423828125 };
-  const parsedLatitude = convertDMSStringToDecimal(latitude) || defaultLatLng.lat;
-  const parsedLongitude = convertDMSStringToDecimal(longitude) || defaultLatLng.lng;
+const MapComponent = ({ mapUrl, latitude, longitude }) => {
+  if (!mapUrl) return null;
 
-  const [markerPosition, setMarkerPosition] = useState({
-    lat: parsedLatitude ?? defaultLatLng.lat,
-    lng: parsedLongitude ?? defaultLatLng.lng,
-  });
-  const [center, setCenter] = useState({
-    lat: parsedLatitude,
-    lng: parsedLongitude,
-  });
-  const [zoom, setZoom] = useState(18);
+  const handleClick = useCallback(() => {
+    const googleMapsUrl =
+      latitude && longitude ? `https://www.google.com/maps?q=${latitude},${longitude}` : mapUrl;
 
-  const handleMarkerClick = useCallback(() => {
-    const googleMapsUrl = `https://www.google.com/maps?q=${markerPosition.lat},${markerPosition.lng}`;
     window.open(googleMapsUrl, '_blank');
-  }, [markerPosition]);
-
-  useEffect(() => {
-    const newLat = convertDMSStringToDecimal(latitude) ?? defaultLatLng.lat;
-    const newLng = convertDMSStringToDecimal(longitude) ?? defaultLatLng.lng;
-
-    if (newLat !== markerPosition.lat || newLng !== markerPosition.lng) {
-      const newCenter = { lat: newLat, lng: newLng };
-      setMarkerPosition(newCenter);
-      setCenter(newCenter);
-    }
-  }, [latitude, longitude, markerPosition.lat, markerPosition.lng]);
+  }, [latitude, longitude, mapUrl]);
 
   return (
-    <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-      <Map
-        defaultZoom={5}
-        mapId={process.env.REACT_APP_GOOGLE_MAPS_ID}
-        style={{ height: '216px', width: '100%' }}
-        zoom={zoom}
-        defaultCenter={center}
-        center={center}
-        onCameraChanged={(ev) => {
-          setZoom(ev?.detail?.zoom);
-          setCenter(ev?.detail?.center);
-        }}
-        options={{ fullscreenControl: false }}
-      >
-        <AdvancedMarker position={markerPosition} draggable={false} onClick={handleMarkerClick}>
-          <Pin />
-        </AdvancedMarker>
-      </Map>
-    </APIProvider>
+    <img
+      src={mapUrl}
+      alt="Static map showing selected location"
+      style={{ borderRadius: '8px', height: '216px', width: '100%', cursor: 'pointer' }}
+      className="google-static-map"
+      onClick={handleClick}
+    />
   );
 };
 
