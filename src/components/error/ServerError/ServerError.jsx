8ced206493565e6f9b55873db443c27cd1d39766
Abhishek P Anil <abhishek@culturecreates.com>
2025-02-21 16:31:30 +0530

    feat: udpated the error page

--- src/components/error/ServerError/ServerError.jsx
+++ src/components/error/ServerError/ServerError.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
 import './serverError.css';
+import { useTranslation } from 'react-i18next';
 
 const ServerError = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="error-container">
-      {/* Illustration */}
-      <img src="/error-illustration.png" alt="Error Illustration" className="error-image" />
+      <p className="error-heading">{t('errorPage.heading')}</p>
+      <p className="error-subtext">{t('errorPage.subtext')}</p>
 
-      {/* Error Message */}
-      <h2 className="error-heading">Oops, something went wrong!</h2>
-
-      {/* Buttons */}
-      {/* <div className="error-actions"> */}
-      {/* <button className="error-button" onClick={() => window.history.back()}>
-          Back
-        </button> */}
-      {/* <p className="error-text">
-          Can’t get back to where you were?{' '}
-          <span className="error-link" onClick={() => (window.location.href = '/login')}>
-            Exit to login
-          </span>
-        </p> */}
-      {/* </div> */}
+      <button className="error-button">{t('errorPage.homeButton')}</button>
     </div>
   );
 };
