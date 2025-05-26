9293a3add53c4136179039c1f2741b42cf5ba8c4
syam babu <syam@dexlock.com>
2025-05-26 18:03:58 +0530

    fix: made assets path relative

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -16,5 +15,6 @@ export default defineConfig({
         entryFileNames: 'widget.js',
       },
     },
+    base: './',
   },
 });
