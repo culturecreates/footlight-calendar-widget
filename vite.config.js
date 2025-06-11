447ae645d854fde1d16ed3daa87c1f831b05a039
syam babu <syam@dexlock.com>
2025-06-11 13:58:53 +0530

    fix: implemented iife format bundling

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,23 @@ export default defineConfig({
   server: {
     port: 3001,
   },
+  define: {
+    'process.env': {},
+  },
   base: './',
   build: {
     outDir: 'build',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        passes: 2,
+      },
+    },
+    lib: {
+      entry: 'src/index.jsx',
+      name: 'CalendarWidget',
+      formats: ['iife'],
+    },
     rollupOptions: {
       output: {
         assetFileNames: 'static/css/widget.[ext]',
